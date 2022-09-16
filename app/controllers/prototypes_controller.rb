4ecf8eb78e3cc5677c4c726fb05d161b2b9a8a82
utidayudai <s.luv.26.0720@gmail.com>
2022-09-16 10:42:07 +0900

    ユーザー以外がつつけない
    と

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @prototypes =Prototype.includes(:user)
   end
@@ -52,4 +53,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
