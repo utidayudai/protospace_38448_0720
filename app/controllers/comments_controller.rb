2a14096bc633d974c993e41854123e2b1066f317
utidayudai <s.luv.26.0720@gmail.com>
2022-09-16 18:25:04 +0900

    あってるか？

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render "prototypes/show"
+      render :show
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
-  resources :users, only: [:show]
+  resources :users, only: :show
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create 
   end
